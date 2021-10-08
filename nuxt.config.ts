2b8101f895f78e595afe63a1ff782a013a354565
Sébastien Chopin <seb@nuxtjs.com>
2021-10-08 18:15:07 +0200

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt Starter',
+    title: 'Nuxt Bridge Starter',
     htmlAttrs: {
       lang: 'en'
     },
